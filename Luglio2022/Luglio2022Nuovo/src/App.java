4265a7a72fc1c1a526314e474672de4458211c37
Formola <formola.paolo@gmail.com>
2022-10-10 14:41:43 +0200

    luglio2022

--- Luglio2022/Luglio2022Nuovo/src/App.java
+++ Luglio2022/Luglio2022Nuovo/src/App.java
@@ -1,44 +1,137 @@
 import java.util.*;
+import java.util.concurrent.Semaphore;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
-        
+        Faldone faldone = new Faldone();
 
+        Semaphore emptyCapo = new Semaphore(5);
+        Semaphore mutexCapo = new Semaphore(0);
 
+        Capufficio cap = new Capufficio(faldone,emptyCapo, mutexCapo);
 
-        Impiegato imp1 = new Impiegato();
+        Impiegato imp1 = new Impiegato(faldone, emptyCapo, cap, mutexCapo);
         imp1.setName("[Impiegato1]");
         imp1.start();
-        Impiegato imp2 = new Impiegato();
+        Impiegato imp2 = new Impiegato(faldone, emptyCapo, cap, mutexCapo);
         imp2.setName("[Impiegato2]");
         imp2.start();
-        Capufficio cap = new Capufficio();
         cap.setName("[Capufficio]");
         cap.start();
 
     }
 }
 
-class Impiegato extends Thread{
+class Impiegato extends Thread {
 
-    public Impiegato(){
+    Faldone faldone;
+    int id;
+    static int ID = 0;
 
+    LinkedList<Integer> myPratiche;
+    static Semaphore mutexPratiche = new Semaphore(0);
+
+    Semaphore emptyCapo;
+    Capufficio cap;
+    Semaphore mutexCapo;
+
+    public Impiegato(Faldone faldone, Semaphore emptyCapo, Capufficio cap, Semaphore mutexCapo) {
+        this.faldone = faldone;
+        this.id = ID;
+        ID++;
+        myPratiche = new LinkedList<>();
+        this.emptyCapo = emptyCapo;
+        this.cap = cap;
+        this.mutexCapo = mutexCapo;
+
+        if (ID == 2) {
+            mutexPratiche.release();
+        }
     }
 
-    public void run(){
+    public void run() {
+
+        try {
+            while (faldone.pratiche.size() >= 0) {
+
+                mutexPratiche.acquire();
+                faldone.pratiche.removeLast();
+                myPratiche.add(1);
+                System.out.println(getName() + " ha lavorato ad una pratica, ne rimangono " + faldone.pratiche.size());
+                
+                emptyCapo.acquire();
+                cap.pratiche_da_firmare.add(1);
+                mutexCapo.release();
+                myPratiche.removeLast();
+                System.out.println(getName() + " ripone una pratica sulla scrivania del capufficio");
+
+                if ( faldone.pratiche.size() == 0){
+                    mutexPratiche.release();
+                    break;
+                }
+                mutexPratiche.release();
+            }
+        } catch (Exception e) {
+
+        }
+        System.out.println(getName() + " : ha terminato");
 
     }
 }
 
 class Capufficio extends Thread {
 
+    LinkedList<Integer> pratiche_da_firmare;
+    Semaphore emptyCapo;
+    Semaphore mutexCapo;
+    Faldone faldone;
 
-    public Capufficio(){
-
+    public Capufficio(Faldone faldone,Semaphore emptyCapo, Semaphore mutexCapo) {
+        pratiche_da_firmare = new LinkedList<>();
+        this.emptyCapo = emptyCapo;
+        this.mutexCapo = mutexCapo;
+        this.faldone = faldone;
     }
 
-    public void run(){
+    public void run() {
+        try {
 
+            while (pratiche_da_firmare.size() >= 0) {
+                mutexCapo.acquire();
+
+                if (pratiche_da_firmare.size() > 0) {
+                    System.out.println(
+                            getName() + " ci sono " + pratiche_da_firmare.size() + " da firmare sulla scrivania");
+                    pratiche_da_firmare.removeLast();
+                    System.out.println(getName() + " firma una pratica,ne rimangono " + pratiche_da_firmare.size());
+                    emptyCapo.release();
+                } else {
+                    mutexCapo.release();
+                    emptyCapo.release();
+
+                }
+                if ( faldone.pratiche.size() == 0){
+                    break;
+                }
+                
+
+            }
+
+        } catch (Exception e) {
+
+        }
     }
+}
+
+class Faldone {
+
+    LinkedList<Integer> pratiche = new LinkedList<>();
+
+    public Faldone() {
+        for (int i = 0; i < 50; i++) {
+            pratiche.add(i);
+        }
+    }
+
 }
