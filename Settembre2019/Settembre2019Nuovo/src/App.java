9d62849ac239c69d5a0850d95f54fc670fc87d88
Formola <formola.paolo@gmail.com>
2022-10-11 17:14:13 +0200

    settembre19

--- Settembre2019/Settembre2019Nuovo/src/App.java
+++ Settembre2019/Settembre2019Nuovo/src/App.java
@@ -5,13 +5,16 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         Deposito deposito = new Deposito();
-
-        Semaphore mutexA = new Semaphore(1);
+        final int n = 2;
+        Semaphore[] turni = new Semaphore[n];
+        for ( int i = 0; i< n; i++){
+            turni[i] = new Semaphore(0);
+        }
         Semaphore mutexB = new Semaphore(0);
 
-        RobotA robotA1 = new RobotA(deposito,mutexB,mutexA);
-        RobotA robotA2 = new RobotA(deposito,mutexB,mutexA);
-        RobotB robotB = new RobotB(deposito,mutexB,mutexA);
+        RobotA robotA1 = new RobotA(deposito,mutexB,turni);
+        RobotA robotA2 = new RobotA(deposito,mutexB,turni);
+        RobotB robotB = new RobotB(deposito,mutexB,turni);
         robotA1.setName("[RobotA1]");
         robotA2.setName("[RobotA2]");
         robotB.setName("[RobotB]");
@@ -26,38 +29,40 @@ class RobotA extends Thread {
 
     Deposito deposito;
     Semaphore mutexB;
-    Semaphore mutexA;
+    Semaphore[] turni;
     
     static int inserimenti = 0;
     int id;
     static int ID = 0;
+    Random random = new Random();
 
-
-    public RobotA(Deposito deposito,Semaphore mutexB, Semaphore mutexA){
+    public RobotA(Deposito deposito,Semaphore mutexB, Semaphore[] turni){
         this.deposito = deposito;
         this.mutexB = mutexB;
-        this.mutexA = mutexA;
+        this.turni = turni;
         this.id = ID;
         ID++;
+
+        if ( ID == 2){
+            turni[random.nextInt(2)].release();
+        }
     }
 
     public void run(){
         try{
             while(deposito.D1.size() > 0 ){
 
-                mutexA.acquire();
+                turni[id].acquire();
                 deposito.D1.removeLast();
-                deposito.mutexD2.acquire();
                 deposito.D2.add(1);
                 System.out.println(getName() + " estrae un oggetto da D1 e lo inserisce in D2");
-                deposito.mutexD2.release();
 
                 inserimenti++;
                 if ( inserimenti == 2){
                     inserimenti = 0;
                     mutexB.release();
                 } else {
-                    mutexA.release();
+                    turni[(id+1)%2].release();
                 }
 
                 if ( deposito.D1.size() == 0 ) {
@@ -78,12 +83,13 @@ class RobotB extends Thread {
 
     Deposito deposito;
     Semaphore mutexB;
-    Semaphore mutexA;
+    Semaphore[] turni;
+    Random random = new Random();
 
-    public RobotB(Deposito deposito,Semaphore mutexB, Semaphore mutexA){
+    public RobotB(Deposito deposito,Semaphore mutexB, Semaphore[] turni){
         this.deposito = deposito;
         this.mutexB = mutexB;
-        this.mutexA = mutexA;
+        this.turni = turni;
     }
 
     public void run(){
@@ -98,7 +104,7 @@ class RobotB extends Thread {
                 deposito.D1.add(1);
                 System.out.println(getName() + " inserisce un oggetto in D1");
                 deposito.mutexD1.release();
-                mutexA.release();
+                turni[random.nextInt(2)].release();
 
                 if ( deposito.D1.size() == 1 ){
                     System.out.println(getName() + " ultimo oggetto inserito");
